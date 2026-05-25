1bb9f948c26ee319a3f2b3e9a1913ed51db2688f
Kairox003 <alessandronichristian03@gmail.com>
2026-05-25 21:19:24 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,63 @@
+const {
+    Client,
+    GatewayIntentBits,
+    EmbedBuilder,
+    Events
+} = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+const TOKEN = process.env.TOKEN;
+
+const STAFF_ROLE_ID = '1498018738753110217';
+const ASSISTENZA_CHANNEL_ID = 'https://discord.com/channels/1439994680413523980/1508549805121732753';
+
+client.once(Events.ClientReady, () => {
+    console.log('Bot online!');
+});
+
+client.on(Events.MessageCreate, async message => {
+
+    if (message.author.bot) return;
+
+    if (message.content.startsWith('!convoca')) {
+
+        if (!message.member.roles.cache.has(STAFF_ROLE_ID)) {
+            return message.reply('Non sei staff.');
+        }
+
+        const utente = message.mentions.users.first();
+
+        if (!utente) {
+            return message.reply('Tagga un utente.');
+        }
+
+        const canale =
+            message.guild.channels.cache.get(
+                ASSISTENZA_CHANNEL_ID
+            );
+
+        const embed = new EmbedBuilder()
+            .setColor('#111111')
+            .setTitle('📞 Convocazione')
+            .setDescription(
+                `${utente} sei stato convocato da ${message.author}`
+            )
+            .setTimestamp();
+
+        await canale.send({
+            content: `${utente}`,
+            embeds: [embed]
+        });
+
+        await message.reply('Convocazione inviata.');
+    }
+});
+
+client.login(TOKEN);
